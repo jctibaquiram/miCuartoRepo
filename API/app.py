e72802d6e3406cd560fc57c4d813b389ecb61e2e
jctibaquiram09 <jctibaquiram09@gmail.com>
2026-05-14 12:24:51 -0500

    metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -15,6 +15,11 @@ def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
 
+@app.get("/cursosPlatzi")
+def get_cursos():
+    cursos = ["Docker","Bash", "Linux", "Inglés","Python" , "Javascript", "Azure","DevOps"]
+    return cursos
+
 @app.get("/superheroesMarvel")
 def get_superheroes_marvel():
     rows = ["Spider-Man", "Iron Man", "Thor", "Hulk", "Black Widow", "Doctor Strange", "Black Panther", "Captain America"]
